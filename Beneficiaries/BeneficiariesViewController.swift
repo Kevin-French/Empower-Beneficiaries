6e93a02626b5188474e6374aa5f5304db751e051
kevin <KFrench1123@gmail.com>
2024-04-18 07:39:28 -0500

    Added collectionView with list compositional layout and no beneficiares view to BeneficiariesViewController

--- Beneficiaries/BeneficiariesViewController.swift
+++ Beneficiaries/BeneficiariesViewController.swift
@@ -8,10 +8,59 @@
 import UIKit
 
 class BeneficiariesViewController: UIViewController {
+  
+  lazy var collectionView = makeCollectionView()
+  
+  lazy var noBeneficiariesView = makeNoBeneficiariesView()
 
   override func viewDidLoad() {
+    
     super.viewDidLoad()
+    
     view.backgroundColor = .red
+    
     title = "Beneficiaries"
+    
+    setUpLayout()
+  }
+  
+  private func makeCollectionView() -> UICollectionView {
+    let collectionView = UICollectionView(
+      frame: .zero,
+      collectionViewLayout: makeLayout())
+    collectionView.translatesAutoresizingMaskIntoConstraints = false
+    collectionView.backgroundColor = .green
+    return collectionView
+  }
+  
+  private func makeLayout() -> UICollectionViewCompositionalLayout {
+    UICollectionViewCompositionalLayout.list(
+      using: UICollectionLayoutListConfiguration(
+        appearance: .grouped))
+  }
+  
+  private func makeNoBeneficiariesView() -> UIContentUnavailableView {
+    var config = UIContentUnavailableConfiguration.empty()
+    config.image = UIImage(systemName: "person.slash")
+    config.text = "No Beneficiaries"
+    let view = UIContentUnavailableView(configuration: config)
+    view.translatesAutoresizingMaskIntoConstraints = false
+    return view
+  }
+  
+  private func setUpLayout() {
+    
+//    view.addSubview(collectionView)
+    view.addSubview(noBeneficiariesView)
+    
+//    collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+//    collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+//    collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+//    collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+    
+    noBeneficiariesView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+    noBeneficiariesView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+    noBeneficiariesView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+    noBeneficiariesView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
   }
 }
---
+++ BeneficiariesTests/BeneficiariesViewController.swift
@@ -0,0 +1,14 @@
+//
+//  BeneficiariesViewController.swift
+//  BeneficiariesTests
+//
+//  Created by Kevin French on 4/18/24.
+//
+
+import XCTest
+@testable import Beneficiaries
+
+final class BeneficiariesViewController: XCTestCase {
+  
+
+}
