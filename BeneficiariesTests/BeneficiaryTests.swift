d3df2bbee2993c61cae4bf7813fc650780941f34
kevin <KFrench1123@gmail.com>
2024-04-18 17:28:02 -0500

    Added test for Beneficiary fullName property

--- BeneficiariesTests/BeneficiaryTests.swift
+++ BeneficiariesTests/BeneficiaryTests.swift
@@ -11,6 +11,20 @@ import XCTest
 final class BeneficiaryTests: XCTestCase {
   
   func testFullNameString() {
-    
+    let sut = Beneficiary(
+      lastName: "Last",
+      firstName: "First",
+      designation: .contingent,
+      beneficiaryType: .child,
+      socialSecurityNumber: "XXXXX5555",
+      middleName: "J",
+      phoneNumber: "8888888888",
+      beneficiaryAddress: Address(
+        firstLineMailing: "123 Street",
+        city: "City",
+        zipCode: "11111",
+        stateCode: .colorado,
+        country: .unitedStates))
+    XCTAssertEqual(sut.fullName, "First Last")
   }
 }
