1fb458d56a0de9ad5c8f04832557e71627a24a91
kevin <KFrench1123@gmail.com>
2024-04-18 17:34:39 -0500

    Fixed issue with launch screen color not working in dark mode

---
+++ BeneficiariesTests/BeneficiariesViewControllerTests.swift
@@ -0,0 +1,16 @@
+//
+//  BeneficiariesViewControllerTests.swift
+//  BeneficiariesTests
+//
+//  Created by Kevin French on 4/18/24.
+//
+
+import XCTest
+@testable import Beneficiaries
+
+final class BeneficiariesViewControllerTests: XCTestCase {
+  
+  func testSomething() {
+    
+  }
+}
